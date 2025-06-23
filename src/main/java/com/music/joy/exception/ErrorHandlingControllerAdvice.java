f47108a830e2c730a1bbf6435880166b6adc4365
joydeep <joydeep@yukawa.de>
2025-06-23 10:10:54 +0200

    refactoring

--- src/main/java/com/music/joy/exception/ErrorHandlingControllerAdvice.java
+++ src/main/java/com/music/joy/exception/ErrorHandlingControllerAdvice.java
@@ -33,8 +33,8 @@ public class ErrorHandlingControllerAdvice {
     @ExceptionHandler(ArtistNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ResponseBody
-    public ErrorResponse onLongUrlNotFoundException(ArtistNotFoundException exception) {
-        ErrorResponse error = ErrorResponse.builder().status(HttpStatus.NOT_FOUND).debugMessage("Long url not found").build();
+    public ErrorResponse onArtistNotFoundException(ArtistNotFoundException exception) {
+        ErrorResponse error = ErrorResponse.builder().status(HttpStatus.NOT_FOUND).debugMessage("Artist not found").build();
         log.error("ArtistNotFoundException found ", exception);
         return error;
     }
